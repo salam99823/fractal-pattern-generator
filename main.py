0a424237a1fa2a767ab9663f09b1883c0dedaa43
salamat2008 <46232788+salamat2008@users.noreply.github.com>
2024-01-11 14:41:22 +0900

    _

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from PySide6.QtWidgets import QApplication, QMainWindow
 
+from untitled import Ui_MainWindow
+
+
+def main():
+    app = QApplication()
+    win = QMainWindow()
+    Ui_MainWindow().setupUi(win)
+    win.show()
+    app.exec()
+
+
+if __name__ == '__main__':
+    main()
--- untitled.py
+++ untitled.py
@@ -1,4 +1,3 @@
-"""
 # -*- coding: utf-8 -*-
 
 ################################################################################
@@ -8,51 +7,26 @@
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
-"""
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QSize, Qt)
-from PySide6.QtWidgets import (QListView,
-                               QVBoxLayout,
+from PySide6.QtCore import (QCoreApplication, QMetaObject, Qt)
+from PySide6.QtWidgets import (QVBoxLayout,
                                QWidget)
-
-from MWidgets.MList_Widgets.MColor_list_Widget import MColor_list_Widget
 
 
 class Ui_MainWindow(object):
-    centralwidget: QWidget
-    verticalLayout: QVBoxLayout
-    listWidget: MColor_list_Widget
-    
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(507, 427)
+        MainWindow.resize(730, 633)
         MainWindow.setContextMenuPolicy(Qt.NoContextMenu)
         MainWindow.setAcceptDrops(False)
-        
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        
-        self.listWidget = MColor_list_Widget(self.centralwidget)
-        self.listWidget.setObjectName(u"listWidget")
-        self.listWidget.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.listWidget.setSizeAdjustPolicy(MColor_list_Widget.SizeAdjustPolicy.AdjustToContents)
-        self.listWidget.setTabKeyNavigation(True)
-        self.listWidget.setDragDropMode(MColor_list_Widget.DragDropMode.DragDrop)
-        self.listWidget.setDefaultDropAction(Qt.MoveAction)
-        self.listWidget.setAlternatingRowColors(True)
-        self.listWidget.setIconSize(QSize(25, 25))
-        self.listWidget.setMovement(QListView.Movement.Free)
-        
-        self.verticalLayout.addWidget(self.listWidget)
-        
         MainWindow.setCentralWidget(self.centralwidget)
         
         self.retranslateUi(MainWindow)
-        self.listWidget.customContextMenuRequested.connect(self.listWidget.show_menu)
         
         QMetaObject.connectSlotsByName(MainWindow)
     
