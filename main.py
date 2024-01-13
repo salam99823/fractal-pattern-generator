6e90629f5fca98903ffa665d57427536802a2819
salam99823 <46232788+salamat2008@users.noreply.github.com>
2024-01-13 19:15:52 +0900

    Update main.py repair space's

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ class Main_window(QMainWindow):
     class Ui_main_widow(Ui_MainWindow):
         def setupUi(self, MainWindow):
             super().setupUi(MainWindow)
-             self.action_about_Qt.triggered.connect(QApplication.aboutQt)
-             self.action_about_program.triggered.connect(MainWindow.tst)
-             self.rules_list.customContextMenuRequested.connect(self.rules_list.show_menu)
-             self.colors_list.customContextMenuRequested.connect(self.colors_list.show_menu)
+            self.action_about_Qt.triggered.connect(QApplication.aboutQt)
+            self.action_about_program.triggered.connect(MainWindow.tst)
+            self.rules_list.customContextMenuRequested.connect(self.rules_list.show_menu)
+            self.colors_list.customContextMenuRequested.connect(self.colors_list.show_menu)
     def __init__(self):
         super().__init__()
         Ui_main_widow().setupUi(self)
