4dc5b02225bc577496659f493f289f925fe1a79e
salam <salamatbekboev2008@gmal.com>
2024-02-01 23:26:33 +0900

    modified:   MWidgets/MColor_list_Widget.py 	modified:   tests/test_Lsystem.py 	modified:   tests/test_MColor_list_Widget.py

--- my_lib_rs/src/lib.rs
+++ my_lib_rs/src/lib.rs
@@ -1,5 +1,5 @@
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
-use pyo3::PyErr;
 use std::collections::HashMap;
 
 struct Point {
@@ -16,10 +16,10 @@ struct Line {
 impl Line {
     fn end_point(&self) -> Point {
         Point {
-            x_coordinate: self.start_point.x_coordinate +
-                self.length * self.angle.to_radians().cos(),
-            y_coordinate: self.start_point.y_coordinate -
-                self.length * self.angle.to_radians().sin(),
+            x_coordinate: self.start_point.x_coordinate
+                + self.length * self.angle.to_radians().cos(),
+            y_coordinate: self.start_point.y_coordinate
+                - self.length * self.angle.to_radians().sin(),
         }
     }
     fn to_tuple(&self) -> (f64, f64, f64, f64) {
@@ -40,7 +40,6 @@ impl Line {
     }
 }
 
-#[derive(Debug)]
 #[pyclass]
 enum Actions {
     DrawForward,
@@ -60,7 +59,7 @@ impl<'a> FromPyObject<'a> for Actions {
             "Actions.MoveBack" => Actions::MoveBack,
             "Actions.TurnLeft" => Actions::TurnLeft,
             "Actions.TurnRight" => Actions::TurnRight,
-            _ => return Err(PyErr::fetch(obj.py())),
+            _ => return Err(PyTypeError::new_err(format!("Invalid action: {}", obj))),
         };
         Ok(action)
     }
@@ -69,52 +68,55 @@ impl<'a> FromPyObject<'a> for Actions {
 fn is_draw_command(command: &String, command_dict: &HashMap<String, Actions>) -> bool {
     match command_dict.get(command) {
         None => false,
-        Some(command) => {
-            match command {
-                Actions::DrawForward => true,
-                Actions::DrawBack => true,
-                _ => false,
-            }
-        }
+        Some(command) => match command {
+            Actions::DrawForward => true,
+            Actions::DrawBack => true,
+            _ => false,
+        },
     }
 }
 
-fn count_draw_commands(commands: &Vec<(String, isize)>,
-                       command_dict: &HashMap<String, Actions>,
+fn count_draw_commands(
+    commands: &Vec<(String, isize)>,
+    command_dict: &HashMap<String, Actions>,
 ) -> usize {
     let mut count = 0;
-    for (_, quantity) in commands.iter().filter(
-        |(command, _)|
-            is_draw_command(command, command_dict)
-    ) {
+    for (_, quantity) in commands
+        .iter()
+        .filter(|(command, _)| is_draw_command(command, command_dict))
+    {
         count += quantity.abs() as usize;
     }
     count
 }
 
 #[pyfunction]
-fn multiply_recursively(mut string: String,
-                        rules: Vec<(String, String)>,
-                        number_of_iters: usize,
+fn multiply_recursively(
+    mut string: String,
+    rules: Vec<(String, String)>,
+    number_of_iters: usize,
 ) -> PyResult<String> {
     for _ in 0..number_of_iters {
         for (key, value) in rules.iter() {
             string = string.replace(key, value);
-        };
-    };
+        }
+    }
     Ok(string)
 }
 
 #[pyfunction]
-fn generate_lines(_actions: Vec<(String, isize)>,
-                  command_dict: HashMap<String, Actions>,
-                  angle_of_rotation: f64,
+fn generate_lines(
+    _actions: Vec<(String, isize)>,
+    command_dict: HashMap<String, Actions>,
+    angle_of_rotation: f64,
 ) -> PyResult<Vec<(f64, f64, f64, f64)>> {
-    let mut vector: Vec<(f64, f64, f64, f64)> = Vec::with_capacity(
-        count_draw_commands(&_actions, &command_dict)
-    );
+    let mut vector: Vec<(f64, f64, f64, f64)> =
+        Vec::with_capacity(count_draw_commands(&_actions, &command_dict));
     let mut line = Line {
-        start_point: Point { x_coordinate: 0.0, y_coordinate: 0.0 },
+        start_point: Point {
+            x_coordinate: 0.0,
+            y_coordinate: 0.0,
+        },
         length: 100.0,
         angle: 0.0,
     };
