5574fe1fd9a7bd334b2c0fb1fc2820e476282452
salam <salamatbekboev2008@gmail.com>
2024-04-16 16:04:00 +0900

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import "./styles.css";
 import App from "./App.svelte";
 
+let target = document.getElementById("app");
+if (target === null) {
+  target = document.createElement("div");
+  target.id = "app";
+  document.body.appendChild(target);
+}
+
 const app = new App({
-  target: document.getElementById("app"),
+  target: target,
 });
 
 export default app;
