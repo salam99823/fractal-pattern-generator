0ee831ead9cb137d7fe9109b6f90f258a18793f0
salam <salamatbekboev2008@gmail.com>
2024-04-17 16:31:42 +0900

    modified:   index.html modified:   src-tauri/Cargo.lock modified:   src-tauri/Cargo.toml modified:   src-tauri/tauri.conf.json modified:   src/App.svelte new file:   src/lib/MenuBar.svelte modified:   src/styles.css

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import MenuBar from "./lib/MenuBar.svelte";
 </script>
 
+<MenuBar />
 <main class="container">
 </main>
---
+++ src/lib/MenuBar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<div class="menubar">
+  <div class="menubar-conteiner">Файл</div>
+  <div class="menubar-conteiner">Вид</div>
+  <div class="menubar-conteiner">Справка</div>
+</div>
+
+<style>
+  .menubar {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+  }
+
+  .menubar-conteiner {
+    display: flex;
+    padding: 3px;
+    border-radius: 0%;
+    cursor: default;
+    font-size: small;
+  }
+</style>
