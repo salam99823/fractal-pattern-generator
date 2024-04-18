aa678681801a91995cc6aa8d6b5b62cfd8f088d0
salam <salamatbekboev2008@gmail.com>
2024-04-18 13:56:57 +0900

    new file:   src/lib/Canvas.svelte new file:   src/lib/MenuBar/MenuBarButton.svelte new file:   src/lib/MenuBar/index.svelte

---
+++ src/lib/MenuBar/MenuBarButton.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let text: string = "";
+  export let actions: string[] = [];
+  let actionsopen: boolean = false;
+  function openactions() {
+    actionsopen = !actionsopen;
+  }
+</script>
+
+<div class="menubar-btn-block">
+  <button type="button" class="menubar-btn" on:click={openactions}
+    >{text}</button
+  >
+  <menu class="actions" class:show={actionsopen}>
+    {#each actions as action}
+      <li><button type="button" class="menubar-btn">{action}</button></li>
+    {/each}
+  </menu>
+</div>
+
+<style>
+  .menubar-btn {
+    padding: 4px;
+    border-radius: 0;
+    font-size: small;
+  }
+  .actions {
+    display: none;
+    padding: 0;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  .actions *.menubar-btn {
+    display: flex;
+    position: static;
+  }
+  .show {
+    display: contents;
+  }
+  .menubar-btn-block {
+    overflow: visible;
+    display: block;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
---
+++ src/lib/MenuBar/index.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import MenuBarButton from "./MenuBarButton.svelte";
+</script>
+
+<div class="menubar">
+  <MenuBarButton text="Файл" actions={["1", "2"]}/>
+  <MenuBarButton text="Настройки" actions={["1", "2"]}/>
+  <MenuBarButton text="Вид" actions={["1", "2"]}/>
+  <MenuBarButton text="Справка" actions={["1", "2"]}/>
+</div>
+
+<style>
+  .menubar {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start space-around;
+    align-items: center;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: #ffffff;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .menubar {
+      background-color: #0f0f0f98;
+      color: #ffffff;
+    }
+  }
+</style>
